bd39e8e06068bb9ef19bdf94ac2d2c95102326ca
houseparty101 <maxxtandon@gmail.com>
2023-04-15 15:14:14 -0400

    smth smth :)

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
+from datetime import datetime
 
 
 customers = Blueprint('customers', __name__)
@@ -9,8 +10,9 @@ customers = Blueprint('customers', __name__)
 @customers.route('/customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
-        first_name, job_title, business_phone from customers')
+
+    cursor.execute("SELECT * FROM Customers")
+
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -19,13 +21,14 @@ def get_customers():
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
+
     return the_response
 
 # Get customer detail for customer with particular userID
 @customers.route('/customers/<userID>', methods=['GET'])
 def get_customer(userID):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where id = {0}'.format(userID))
+    cursor.execute("SELECT * FROM Customers WHERE CustomerID = %s", (userID,))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -34,4 +37,18 @@ def get_customer(userID):
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
+
+
     return the_response
+
+@customers.route('/customers', methods=['POST'])
+def add_customers():
+    cursor = db.get_db().cursor()
+
+    cust_info = request.json
+
+
+
+    query = "INSERT INTO Customers (FirstName, LastName, PhoneNumber, Email, DOB, Street, City, State, ZipCode, Country)"
+
+    return
---
+++ flask-app/src/tickets/tickets.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+
+tickets = Blueprint('tickets', __name__)
+
+# Get all the tickets from the database
+@tickets.route('/tickets', methods=['GET'])
+def get_products():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of tickets
+    cursor.execute('SELECT * FROM Tickets')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+@tickets.route('/tickets/<ticketID>', methods=['GET'])
+def get_ticket(ticketID):
+    cursor = db.get_db().cursor()
+
+
+    query = "SELECT * FROM Tickets WHERE TicketID = %s"
+
+    cursor.execute(query, (ticketID))
+       # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
