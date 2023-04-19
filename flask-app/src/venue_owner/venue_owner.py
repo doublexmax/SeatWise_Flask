a518d23eb8558ede4dd772311256a94c0dd36143
Khanh-Kayden <98710686+Khanh-Kayden@users.noreply.github.com>
2023-04-19 17:33:04 -0400

    Update Venue Owner

--- flask-app/src/venue_owner/venue_owner.py
+++ flask-app/src/venue_owner/venue_owner.py
@@ -28,22 +28,49 @@ def get_owner_venues(VenueOwnerID):
 
     return the_response
 
-# Add a new venue owner
-@venue_owner.route('/venue_owner', methods=['POST'])
-def add_venue_owner():
+# Get All Venue Owner First + Last Name
+@venue_owner.route('/venue_owner/name', methods=['GET'])
+def get_venue_owner_names():
     cursor = db.get_db().cursor()
 
-    vo_info = request.json
-
-    vo_tuple = f"('{vo_info.get('VOFirstName')}', '{vo_info.get('VOLastName')}', '{vo_info.get('VOPhone')}', '{vo_info.get('VOEmail')}')"
-
-    query = f"INSERT INTO VenueOwner (FirstName, LastName, PhoneNumber, Email) VALUES {vo_tuple}"
+    query = "SELECT CONCAT(FirstName, ' ', LastName) as label, OwnerID as value FROM VenueOwner"
 
     cursor.execute(query)
 
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+
     db.get_db().commit()
 
-    return "Successfully added new Venue Owner"
+    return the_response
+
+# Get All Info of a Venue Owner
+@venue_owner.route('/venue_owner/<voIDinfo>/info', methods=['GET'])
+def get_venue_owner_email(voIDinfo):
+    cursor = db.get_db().cursor()
+
+    query = "SELECT FirstName, LastName, PhoneNumber, Email FROM VenueOwner WHERE OwnerID = %s"
+
+    cursor.execute(query, (voIDinfo,))
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+
+    db.get_db().commit()
+
+    return the_response
 
 # Get All Venue Owner
 @venue_owner.route('/venue_owner')
@@ -67,50 +94,22 @@ def get_venue_owners():
 
     return the_response
 
-# Get All Venue Owner First + Last Name
-@venue_owner.route('/venue_owner/name', methods=['GET'])
-def get_venue_owner_names():
+# Add a new venue owner
+@venue_owner.route('/venue_owner', methods=['POST'])
+def add_venue_owner():
     cursor = db.get_db().cursor()
 
-    query = "SELECT CONCAT(FirstName, ' ', LastName) as label, OwnerID as value FROM VenueOwner"
+    vo_info = request.json
+
+    vo_tuple = f"('{vo_info.get('VOFirstName')}', '{vo_info.get('VOLastName')}', '{vo_info.get('VOPhone')}', '{vo_info.get('VOEmail')}')"
+
+    query = f"INSERT INTO VenueOwner (FirstName, LastName, PhoneNumber, Email) VALUES {vo_tuple}"
 
     cursor.execute(query)
 
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-
     db.get_db().commit()
 
-    return the_response
-
-
-# Get All Info of a Venue Owner
-@venue_owner.route('/venue_owner/<voIDinfo>/info', methods=['GET'])
-def get_venue_owner_email(voIDinfo):
-    cursor = db.get_db().cursor()
-
-    query = "SELECT FirstName, LastName, PhoneNumber, Email FROM VenueOwner WHERE OwnerID = %s"
-
-    cursor.execute(query, (voIDinfo,))
-
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-
-    db.get_db().commit()
-
-    return the_response
+    return "Successfully added new Venue Owner"
 
 # Update the Info of the specified Venue Owner
 @venue_owner.route('/venue_owner/<VenueOwnerID>', methods=['PUT'])
