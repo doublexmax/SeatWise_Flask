00dd9448b45a3015ea0e34e3f77a770531f3cf03
anyueow <anyushah@gmail.com>
2023-04-19 17:35:25 -0400

    Arranging all routes for Customer

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -5,6 +5,13 @@ from datetime import datetime
 
 customers = Blueprint('customers', __name__)
 
+'''
+
+-----------------------------------------
+
+All GET Routes for Customer Blueprint
+
+'''
 # Get all customers from the DB
 @customers.route('/customers', methods=['GET'])
 def get_customers():
@@ -61,6 +68,7 @@ def get_customer(userID):
 # Get customer ticket detail for customer with particular userID
 @customers.route('/customers/<userID>/tickets', methods=['GET'])
 def get_customer_ticket(userID):
+
     cursor = db.get_db().cursor()
 
     query = "SELECT VenueName, Description, Date, Seat_Row, Seat_Column, Section FROM \
@@ -79,81 +87,7 @@ def get_customer_ticket(userID):
 
     return the_response
 
-@customers.route('/customers', methods=['POST'])
-def add_customer():
-    cursor = db.get_db().cursor()
-
-    cust_info = request.json
-
-    cust_tuple = f"('{cust_info.get('CusFirstName', 'NULL')}', '{cust_info.get('CusLastName')}', '{cust_info.get('CusPhone')}', '{cust_info.get('CusEmail')}', '{cust_info.get('CusDOB')[:10]}', \
-        '{cust_info.get('CusStreet')}', '{cust_info.get('CustCity')}', '{cust_info.get('CustState')}', '{cust_info.get('CustZipcode')}', '{cust_info.get('CustCountry')}')"
-    
-    query = f"INSERT INTO Customers (FirstName, LastName, PhoneNumber, Email, DOB, Street, City, State, ZipCode, Country) VALUES {cust_tuple}"
-
-    cursor.execute(query)
-
-    db.get_db().commit()
-
-    return "Successfully added user into database."
-
-@customers.route('/customers/<customerID>/dependents', methods=['POST'])
-def add_customer_dependent(customerID):
-    cursor = db.get_db().cursor()
-
-    cust_info = request.json
-
-    cust_tuple = f"('{cust_info.get('DepFirstName', 'NULL')}', '{cust_info.get('DepLastName')}', '{cust_info.get('DepPhoneCopy')}', '{cust_info.get('DepEmail')}', '{cust_info.get('Relationship')}', \
-        '{cust_info.get('DepStreet')}', '{cust_info.get('DepCity')}', '{cust_info.get('DepState')}', '{cust_info.get('DepZipcode')}', '{cust_info.get('DepCountry')}', {customerID})"
-    
-    query = f"INSERT INTO Dependent (FirstName, LastName, PhoneNumber, Email, Relationship, Street, City, State, ZipCode, Country, Parent) VALUES {cust_tuple}"
-
-    cursor.execute(query)
-
-    db.get_db().commit()
-
-    return "Successfully added dependent."
-
-@customers.route('/customers/<userID>/remove_dependent/<dependentID>', methods=['DELETE'])
-def delete_dependent(userID, dependentID):
-    cursor = db.get_db().cursor()
-    
-    query = f"UPDATE Dependent SET FirstName = 'unavailable', LastName = 'unavailable', \
-        PhoneNumber = NULL, Email = NULL, Street = NULL, City = NULL, State  = NULL, \
-            Zipcode  = NULL, Country  = NULL, Relationship = 'unavailable' WHERE Parent = %s and DependentID  = %s"
-    
-    cursor.execute(query, (userID, dependentID))
-
-    db.get_db().commit()
-
-    return "Deleted dependent"
-
-@customers.route('/customers/<userID>', methods =['DELETE'])
-def delete_account(userID):
-    cursor = db.get_db().cursor()
-
-    query = f"UPDATE Customers SET FirstName = 'unavailable', LastName = 'unavailable', \
-        PhoneNumber = NULL, Email = NULL, Street = NULL, City = NULL, State  = NULL, \
-            Zipcode = NULL, Country = NULL WHERE CustomerID = %s"
-    
-    cursor.execute(query, (userID,))
-
-    db.get_db().commit()
-    
-    return "Deleted customer account"
-
-
-@customers.route('/customers/<userID>/assign_ticket/<ticketID>', methods=['PUT'])
-def assign_ticket(userID, ticketID):
-    cursor = db.get_db().cursor()
-
-    query = f"UPDATE Tickets SET CustomerID = %s WHERE TicketID = %s"
-    
-    cursor.execute(query, (userID, ticketID))
-
-    db.get_db().commit()
-
-    return "Successfully assigned ticket"
-
+# Getting dependent info based on CustomerID 
 @customers.route('/customers/<userID>/dependents', methods = ['GET'])
 def get_dependent(userID):
     cursor = db.get_db().cursor()
@@ -195,21 +129,23 @@ def get_customers_info(cIDInf):
 
     return the_response
 
-# Update Customers Info from Edit form
-@customers.route('/customers/<userID>', methods=['PUT'])
-def put_customers(userID):
+# Add artists as favorite artists to the customer
+@customers.route('/customers/fanof', methods = ['GET'])
+def get_artists_name():
     cursor = db.get_db().cursor()
 
-    c_info = request.json
+    cursor.execute("SELECT CONCAT(FirstName, ' ', LastName) as label, ArtistID as value FROM Artists")
 
-    query = f"UPDATE Customers SET FirstName = '{c_info.get('VOFirstNameCopy')}', LastName = '{c_info.get('VOLastNameCopy')}', \
-    PhoneNumber = '{c_info.get('VOPhoneCopy')}', Email = '{c_info.get('VOEmailCopy')}' WHERE CustomerID = %s"
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
 
-    cursor.execute(query, (userID,))
-
-    db.get_db().commit()
-
-    return "Success!"
+    return the_response
 
 # Editing emergency contact information of Dependent
 @customers.route('/customers/dependent/<cIdDInf>/info', methods=['GET'])
@@ -233,6 +169,125 @@ def get_dependents_info(cIdDInf):
 
     return the_response
 
+'''
+-----------------------------------------
+
+ All POST routes for Customer Blueprint
+'''
+
+# Adding a new Customer 
+@customers.route('/customers', methods=['POST'])
+def add_customer():
+    cursor = db.get_db().cursor()
+
+    cust_info = request.json
+
+    cust_tuple = f"('{cust_info.get('CusFirstName', 'NULL')}', '{cust_info.get('CusLastName')}', '{cust_info.get('CusPhone')}', '{cust_info.get('CusEmail')}', '{cust_info.get('CusDOB')[:10]}', \
+        '{cust_info.get('CusStreet')}', '{cust_info.get('CustCity')}', '{cust_info.get('CustState')}', '{cust_info.get('CustZipcode')}', '{cust_info.get('CustCountry')}')"
+    
+    query = f"INSERT INTO Customers (FirstName, LastName, PhoneNumber, Email, DOB, Street, City, State, ZipCode, Country) VALUES {cust_tuple}"
+
+    cursor.execute(query)
+
+    db.get_db().commit()
+
+    return "Successfully added user into database."
+
+# Adding a dependent for a customer 
+@customers.route('/customers/<customerID>/dependents', methods=['POST'])
+def add_customer_dependent(customerID):
+    cursor = db.get_db().cursor()
+
+    cust_info = request.json
+
+    cust_tuple = f"('{cust_info.get('DepFirstName', 'NULL')}', '{cust_info.get('DepLastName')}', '{cust_info.get('DepPhoneCopy')}', '{cust_info.get('DepEmail')}', '{cust_info.get('Relationship')}', \
+        '{cust_info.get('DepStreet')}', '{cust_info.get('DepCity')}', '{cust_info.get('DepState')}', '{cust_info.get('DepZipcode')}', '{cust_info.get('DepCountry')}', {customerID})"
+    
+    query = f"INSERT INTO Dependent (FirstName, LastName, PhoneNumber, Email, Relationship, Street, City, State, ZipCode, Country, Parent) VALUES {cust_tuple}"
+
+    cursor.execute(query)
+
+    db.get_db().commit()
+
+    return "Successfully added dependent."
+
+# Submitting favourite artist
+@customers.route('/customers/artist/fanof/<CidAid>', methods=['POST'])
+def add_favourite_artist(CidAid):
+    
+    cursor = db.get_db().cursor()
+
+    artist_info = request.json
+
+    for artist_ID in artist_info:
+        query = f"INSERT INTO Fan_Of (CustomerID, ArtistID) VALUES ({CidAid}, {artist_ID})"
+
+        cursor.execute(query)
+
+    db.get_db().commit()
+
+    return "Successfully added Favourite Artists"
+
+'''
+-----------------------------------------
+
+ All DELETE routes for Customer Blueprint
+'''
+
+# Deleting Dependent
+@customers.route('/customers/<userID>/remove_dependent/<dependentID>', methods=['DELETE'])
+def delete_dependent(userID, dependentID):
+    cursor = db.get_db().cursor()
+    
+    query = f"UPDATE Dependent SET FirstName = 'unavailable', LastName = 'unavailable', \
+        PhoneNumber = NULL, Email = NULL, Street = NULL, City = NULL, State  = NULL, \
+            Zipcode  = NULL, Country  = NULL, Relationship = 'unavailable' WHERE Parent = %s and DependentID  = %s"
+    
+    cursor.execute(query, (userID, dependentID))
+
+    db.get_db().commit()
+
+    return "Deleted dependent"
+
+# Deleteing customer account 
+@customers.route('/customers/<userID>', methods =['DELETE'])
+def delete_account(userID):
+    cursor = db.get_db().cursor()
+
+    query = f"UPDATE Customers SET FirstName = 'unavailable', LastName = 'unavailable', \
+        PhoneNumber = NULL, Email = NULL, Street = NULL, City = NULL, State  = NULL, \
+            Zipcode = NULL, Country = NULL WHERE CustomerID = %s"
+    
+    cursor.execute(query, (userID,))
+
+    db.get_db().commit()
+    
+    return "Deleted customer account"
+
+
+'''
+-----------------------------------------
+
+ All PUT routes for Customer Blueprint
+'''
+
+# Update Customers Info from Edit form
+@customers.route('/customers/<userID>', methods=['PUT'])
+def put_customers(userID):
+    cursor = db.get_db().cursor()
+
+    c_info = request.json
+
+    query = f"UPDATE Customers SET FirstName = '{c_info.get('VOFirstNameCopy')}', LastName = '{c_info.get('VOLastNameCopy')}', \
+    PhoneNumber = '{c_info.get('VOPhoneCopy')}', Email = '{c_info.get('VOEmailCopy')}' WHERE CustomerID = %s"
+
+    cursor.execute(query, (userID,))
+
+    db.get_db().commit()
+
+    return "Success!"
+
+
 # Update Customer's Dependent Info from Edit form
 @customers.route('/customers/dependent/<userID>', methods=['PUT'])
 def put_customers_dependent(userID):
@@ -251,37 +306,14 @@ def put_customers_dependent(userID):
 
     return "Success!"
 
-# Add artists as favorite artists to the customer
-@customers.route('/customers/fanof', methods = ['GET'])
-def get_artists_name():
+@customers.route('/customers/<userID>/assign_ticket/<ticketID>', methods=['PUT'])
+def assign_ticket(userID, ticketID):
     cursor = db.get_db().cursor()
 
-    cursor.execute("SELECT CONCAT(FirstName, ' ', LastName) as label, ArtistID as value FROM Artists")
-
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-
-    return the_response
-
-# Submitting favourite artist
-@customers.route('/customers/artist/fanof/<CidAid>', methods=['POST'])
-def add_favourite_artist(CidAid):
+    query = f"UPDATE Tickets SET CustomerID = %s WHERE TicketID = %s"
     
-    cursor = db.get_db().cursor()
-
-    artist_info = request.json
-
-    for artist_ID in artist_info:
-        query = f"INSERT INTO Fan_Of (CustomerID, ArtistID) VALUES ({CidAid}, {artist_ID})"
-
-        cursor.execute(query)
+    cursor.execute(query, (userID, ticketID))
 
     db.get_db().commit()
 
-    return "Successfully added Favourite Artists"
+    return "Successfully assigned ticket"
