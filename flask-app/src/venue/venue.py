88776abe8fabe7824e0d0381af68fcc9c6709d35
houseparty101 <maxxtandon@gmail.com>
2023-04-19 17:31:09 -0400

    organization

--- flask-app/src/venue/venue.py
+++ flask-app/src/venue/venue.py
@@ -1,29 +1,8 @@
 from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
-from datetime import datetime
 
 venue = Blueprint('venue', __name__)
-
-# Add a new venue for the given venue owner 
-@venue.route('/venues/<OwnerID>', methods=['POST'])
-def add_venue(OwnerID):
-    cursor = db.get_db().cursor()
-
-    venue_info = request.json
-
-    venue_tuple = f"('{venue_info.get('NewVenueName')}', '{venue_info.get('NewVenuePhone')}', '{venue_info.get('NewVenueEmail')}', \
-                        '{venue_info.get('NewVenueStreet')}', '{venue_info.get('NewVenueCity')}', \
-                        '{venue_info.get('NewVenueState')}', '{venue_info.get('NewVenueZipcode')}', '{venue_info.get('NewVenueCountry')}', \
-                        '{venue_info.get('Venue_Owner')}')"
-
-    query = f"INSERT INTO Venues (VenueName, PhoneNumber, Email, Street, City, State, Zipcode, Country, OwnerID) VALUES {venue_tuple}"
-
-    cursor.execute(query)
-
-    db.get_db().commit()
-
-    return "Successfully added new Venue"
 
 # Get all the tickets of the given venue
 @venue.route('/venues/<VenueID>/tickets', methods=['GET'])
@@ -33,6 +12,26 @@ def get_tickets(VenueID):
     query = f"SELECT Type, Section, Seat_Row, Seat_Column, IFNULL(CustomerID, \"Available\") AS Customer FROM Tickets NATURAL JOIN Venues WHERE VenueID = %s"
 
     cursor.execute(query, (VenueID,))
+
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+
+    return the_response
+
+# Return all the venue info given the venue owner id
+@venue.route('/venues/<OwnerIDinfo>/info', methods=['GET'])
+def get_venues_info(OwnerIDinfo):
+    cursor = db.get_db().cursor()
+
+    query = "SELECT Email, PhoneNumber, Street, State, City, Zipcode, Country FROM Venues WHERE OwnerID = %s"
+
+    cursor.execute(query, (OwnerIDinfo,))
 
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -64,20 +63,6 @@ def get_revenueo(VenueID):
     the_response.mimetype = 'application/json'
 
     return the_response
-
-# This is to delete the venue by a venue owner
-@venue.route('/venues/<VenueID>', methods=['DELETE'])
-def delete_venue(VenueID):
-    cursor = db.get_db().cursor()
-
-    query = f"UPDATE Venues SET PhoneNumber = 'Unavailable', Email = 'Unavailable', Street = 'Unavailable', City = 'Unavailable', \
-        State = 'Unavailable', Zipcode = 'Unavailable', Country = 'Unavailable' WHERE VenueID = %s"
-
-    cursor.execute(query, (VenueID,))
-
-    db.get_db().commit()
-
-    return "Deleted"
 
 # This is to get all the performances of the given Venue
 @venue.route('/venues/<VenueID>/performances', methods=['GET'])
@@ -121,7 +106,7 @@ def get_venues():
 
     return the_response
 
-# Get Ticket Info and Ticket ID of the given performance
+# Get The Ticket Info and Ticket ID of all unsold tickets of a given performance
 @venue.route('/tickets/<performanceID>/form', methods=['GET'])
 def get_ticket(performanceID):
     cursor = db.get_db().cursor()
@@ -146,26 +131,6 @@ def get_ticket(performanceID):
 
     return jsonify(json_data)
 
-# Return all the venue info given the venue owner id
-@venue.route('/venues/<OwnerIDinfo>/info', methods=['GET'])
-def get_venues_info(OwnerIDinfo):
-    cursor = db.get_db().cursor()
-
-    query = "SELECT Email, PhoneNumber, Street, State, City, Zipcode, Country FROM Venues WHERE OwnerID = %s"
-
-    cursor.execute(query, (OwnerIDinfo,))
-
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-
-    return the_response
-
 # Update the Venue Info of the given Venue Owner
 @venue.route('/venues/<OwnerID>', methods=['PUT'])
 def put_venues(OwnerID):
@@ -183,3 +148,37 @@ def put_venues(OwnerID):
     db.get_db().commit()
 
     return "Success!"
+
+# Add a new venue for the given venue owner 
+@venue.route('/venues/<OwnerID>', methods=['POST'])
+def add_venue(OwnerID):
+    cursor = db.get_db().cursor()
+
+    venue_info = request.json
+
+    venue_tuple = f"('{venue_info.get('NewVenueName')}', '{venue_info.get('NewVenuePhone')}', '{venue_info.get('NewVenueEmail')}', \
+                        '{venue_info.get('NewVenueStreet')}', '{venue_info.get('NewVenueCity')}', \
+                        '{venue_info.get('NewVenueState')}', '{venue_info.get('NewVenueZipcode')}', '{venue_info.get('NewVenueCountry')}', \
+                        '{venue_info.get('VenueOwnerSelect')}')"
+
+    query = f"INSERT INTO Venues (VenueName, PhoneNumber, Email, Street, City, State, Zipcode, Country, OwnerID) VALUES {venue_tuple}"
+
+    cursor.execute(query)
+
+    db.get_db().commit()
+
+    return "Successfully added new Venue"
+
+# This is to delete the venue by a venue owner
+@venue.route('/venues/<VenueID>', methods=['DELETE'])
+def delete_venue(VenueID):
+    cursor = db.get_db().cursor()
+
+    query = f"UPDATE Venues SET PhoneNumber = 'Unavailable', Email = 'Unavailable', Street = 'Unavailable', City = 'Unavailable', \
+        State = 'Unavailable', Zipcode = 'Unavailable', Country = 'Unavailable' WHERE VenueID = %s"
+
+    cursor.execute(query, (VenueID,))
+
+    db.get_db().commit()
+
+    return "Deleted"
